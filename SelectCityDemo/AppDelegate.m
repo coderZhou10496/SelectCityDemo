65a2c59f7123c4862dee33ee0e58b36cd9c7deb4
JianZhou <1049654453@qq.com>
2016-05-27 15:59:43 +0800

    添加gif图片

--- SelectCityDemo/AppDelegate.m
+++ SelectCityDemo/AppDelegate.m
@@ -16,6 +16,7 @@
 
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+    // 自己实现的城市列表选择界面
     // Override point for customization after application launch.
     return YES;
 }
